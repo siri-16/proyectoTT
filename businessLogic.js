cc1403af51845110fbe21b9eaec29b8430884d30
karen <my_skate@outlook.com>
2021-06-09 17:16:58 -0500

    Dos archivos de javascript para objetos

---
+++ businessLogic.js
@@ -0,0 +1,75 @@
+//bussinesLogic.js
+//informacion de los objetos y funcionalidades propias del registro
+
+var clienteList = [];
+
+function addClienteToSystem(pnombreComercio, pdescripcion, null, true,  null, null, pcontrasenia, pemail, pnombreUsuario, prfc, papellidoMaterno, papellidoPaterno, pprimerNombre, prfc, psegundoNombre, palgoritmo, pcifrado, pcontrato, pcostoServicioAnual, pdescripcion, null, null, true, pnumeroContrato, prfc, ptipoServicio,  pcalle, pcodigoPostal, pdelegacionMunicicpio, pidDireccion, prfc, pestado, pnumero, null){
+
+	var newCliente = {
+		var newComercio = {
+		nombreComercio : pnombreComercio,
+		descripcion : pdescripcion,
+		fechaCreacion : null,
+		habilitado : true,
+		fechaAcualizacion :  null;
+		
+		var tokenPANlist : null,
+		
+		var usuarioList = {
+			contrasenia : pcontrasenia,
+			email : pemail,
+			usuarioPK : {
+				nombreUsuario : pnombreUsuario,
+				rfc : prfc
+		 
+		},
+		
+		var newPersona = {
+		apellidoMaterno : papellidoMaterno,
+		apellidoPaterno : papellidoPaterno,
+		primerNombre : pprimerNombre,
+		rfc : prfc,
+		segundoNombre : psegundoNombre
+		
+		var servicioList = {
+		algoritmo : palgoritmo,
+		cifrado : pcifrado,
+		contrato : pcontrato,
+		costoServicioAnual : pcostoServicioAnual,
+		descripcion : pdescripcion,
+		fechaAcualizacion :  null;
+		fechaCreacion : null,
+		habilitado : true,
+		servicioPK = {
+			numeroContrato : pnumeroContrato,
+			rfc : prfc
+		
+		
+		tipoServicio : ptipoServicio
+		
+		
+		var direccionList = {
+		calle : pcalle,
+		codigoPostal : pcodigoPostal
+		delegacionMunicicpio : pdelegacionMunicicpio
+		direccionPK = {
+			idDireccion : pidDireccion,
+			rfc : prfc
+		estado : pestado,
+		numero : pnumero
+				
+		comercio : null
+						
+	};
+	console.log(newCliente);
+	clienteList.push(newCliente);
+}
+
+
+function getClientesList){
+	return clientesList;
+}
+
+
+
+
