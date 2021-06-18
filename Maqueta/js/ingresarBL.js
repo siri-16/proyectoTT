ab192dc48609a8f11aa65e57cf822bd96f509a06
Iris <sirioel_np_16@hotmail.com>
2021-06-18 01:24:24 -0500

    Ingresar con validación de usuarios

--- Maqueta/js/ingresarBL.js
+++ Maqueta/js/ingresarBL.js
@@ -1,17 +1,30 @@
-const url= 'http://localhost:8088/Tokenizer/Clientes/%20/Usuarios/'
+const url= 'http://localhost:8088/Tokenizer/Clientes/%20/Usuarios/list'
 
 function consultarUsuario(pNombreUsuario, pContrasenia){
     var newUsuario = {
-        "usuarioList": [
-            {
                 "contrasenia": pContrasenia,
                 "email": null,
                 "usuarioPK": {
                     "nombreUsuario": pNombreUsuario,
-                    "rfc": prfc
+                    "rfc": null
                 }
-            }
-        ]
     }
 
+    fetch(url)
+        .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
+        .then(res => res.json())
+        //.then(data => console.log(data))
+        .then(res => {
+            for (const userInfo of res) {
+                var user= userInfo.usuarioPK.nombreUsuario;
+                var pass= userInfo.contrasenia;
+                if(user != pNombreUsuario && pass != pContrasenia){
+                    console.log("USUARIO NO REGISTRADO");
+                }else{
+                    console.log("USUARIO REGISTRADO");
+                    window.location="index.html";
+                }
+                
+            }
+        })
 }
--- Maqueta/js/ingresarUILogic.js
+++ Maqueta/js/ingresarUILogic.js
@@ -1,9 +1,7 @@
 document.querySelector('#buttonIngreso').addEventListener('click', verificarUC)
 
-
 function verificarUC(){
     var sNombreUsuario = document.querySelector('#exampleInputUser').value,
         sContrasenia = document.querySelector('#exampleInputPassword').value
-
         consultarUsuario(sNombreUsuario, sContrasenia);
 }
